1b26a77822e6f8a1bcfc49eba86279167875563b
Jeongbeom Kim <jeongbeomkim@Jeongbeomui-MacBookPro.local>
2020-07-13 22:07:02 +0900

    update

--- python_file.py
+++
@@ -1,7 +0,0 @@
-
-###markdown, this notebook will mainly used for Capstone Project for IBM Data Science Course on Coursera
-
-import pandas as pd
-import numpy as np
-
-print("Hello Capston Project Course!") 
