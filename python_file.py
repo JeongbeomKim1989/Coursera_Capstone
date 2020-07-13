cb0c80a41fe491e52d08dd4f07977988a2f948d9
Jeongbeom Kim <jeongbeomkim@Jeongbeomui-MacBookPro.local>
2020-07-13 21:53:12 +0900

    add python script that print a message greeting Capstone Project

---
+++ python_file.py
@@ -0,0 +1,7 @@
+
+###markdown, this notebook will mainly used for Capstone Project for IBM Data Science Course on Coursera
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capston Project Course!") 
